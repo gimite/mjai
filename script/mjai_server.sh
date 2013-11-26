f81a1915542709cc7589c7a6d5f5ee0872e7f121
Hiroshi Ichikawa <gimite@gmail.com>
2013-11-26 18:53:35 +0900

    Updating path to mjai-manue.

--- script/mjai_server.sh
+++ script/mjai_server.sh
@@ -2,10 +2,10 @@
 
 while true; do
   bundle exec ./bin/mjai server \
-    --host=0.0.0.0 --port=11600 --game_type=one_kyoku --room=manue-1kyoku --log_dir=./log --repeat \
-    ../mjai-manue/bin/mjai-manue \
-    ../mjai-manue/bin/mjai-manue \
-    ../mjai-manue/bin/mjai-manue \
+    --host=0.0.0.0 --port=11600 --game_type=one_kyoku --room=manue-1kyoku --log_dir=./log \
+    ../mjai-manue/coffee/mjai-manue \
+    ../mjai-manue/coffee/mjai-manue \
+    ../mjai-manue/coffee/mjai-manue \
     >> log/mjai_server.log 2>&1
   sleep 60
 done
